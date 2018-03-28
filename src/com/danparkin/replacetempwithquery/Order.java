c29d91069ccd1c70ceeed79203ca610233b666cc
Daniel Parkin <danielparkin1@gmail.com>
2018-03-28 21:32:32 +0100

    add replace temp with query

---
+++ src/com/danparkin/replacetempwithquery/Order.java
@@ -0,0 +1,19 @@
+package com.danparkin.replacetempwithquery;
+
+public class Order {
+    private int _itemPrice;
+    private int _quantity;
+
+    public Order(int itemPrice, int quantity) {
+        _itemPrice = itemPrice;
+        _quantity = quantity;
+    }
+
+    public double getPrice() {
+        int basePrice = _quantity * _itemPrice;
+        double discountFactor;
+        if (basePrice > 1000) discountFactor = 0.95;
+        else discountFactor = 0.98;
+        return basePrice * discountFactor;
+    }
+}
