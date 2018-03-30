27c5e7fbe11bbc115829459efcd7b3bb69cbd94f
Daniel Parkin <danielparkin1@gmail.com>
2018-03-30 14:59:49 +0100

    add replace method with method object

---
+++ src/com/danparkin/replacemethodwithmethodobject/README.md
@@ -0,0 +1,9 @@
+# Replace Method with Method Object
+
+## Problem
+You have a long method that uses local variables in such a way that you cannot apply Extract Method.
+
+## Solution
+Turn the method into its own object so that all the local variables become fields on that object. You can then decompose the method into other methods on the same object.
+
+Page 135 - Martin Fowler's "Refactoring - Improving The Design of Existing Code".
---
+++ src/test/ReplaceMethodWithMethodObjectTests.java
@@ -0,0 +1,33 @@
+import com.danparkin.replacemethodwithmethodobject.Account;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+// Note: this method is nonsensical and the tests are merely here to categorize it.
+// Martin Fowler provides a disclaimer for the gamma method in the book:
+//  "Don't ask what the logic of this method is, I made it up as I went along".
+public class ReplaceMethodWithMethodObjectTests {
+    @Test
+    public void gamma_when_year_to_date_minus_important_value_1_less_than_100() {
+        Account account = new Account();
+        int inputVal = 10;
+        int quantity = 5;
+        int yearToDate = 10000;
+        int actualGamma = account.gamma(inputVal, quantity, yearToDate);
+        int expectedGama = 700260;
+
+        assertEquals(expectedGama, actualGamma);
+    }
+
+    @Test
+    public void gamma_when_year_to_date_minus_important_value_1_greater_than_100() {
+        Account account = new Account();
+        int inputVal = 100;
+        int quantity = 110;
+        int yearToDate = 10000;
+        int actualGamma = account.gamma(inputVal, quantity, yearToDate);
+        int expectedGama = 6978500;
+
+        assertEquals(expectedGama, actualGamma);
+    }
+}
