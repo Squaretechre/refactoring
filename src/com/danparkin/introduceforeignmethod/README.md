18a92b4abc3a03e2fc0557637803cdc1175d3362
Daniel Parkin <danielparkin1@gmail.com>
2018-04-14 14:01:16 +0100

    add introduce foreign method

---
+++ src/com/danparkin/introduceforeignmethod/README.md
@@ -0,0 +1,11 @@
+# Introduce Foreign Method
+
+## Moving Features Between Objects 
+
+### Problem
+A server class you are using needs an additional method, but you can't modify the class.
+
+### Solution
+Create a method in the client class with an instance of the server class as its first argument.
+
+Page 162 - Martin Fowler's "Refactoring - Improving The Design of Existing Code".
---
+++ src/test/IntroduceForeignMethodTests.java
@@ -0,0 +1,15 @@
+import com.danparkin.introduceforeignmethod.BillingPeriod;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class IntroduceForeignMethodTests {
+    @Test
+    public void next_day_increments_date_by_1_day() {
+        Date expectedDate = new Date(2018, 04, 14);
+        Date actualDate = new BillingPeriod(new Date(2018, 04, 13)).rollOver();
+        assertEquals(expectedDate.toString(), actualDate.toString());
+    }
+}
