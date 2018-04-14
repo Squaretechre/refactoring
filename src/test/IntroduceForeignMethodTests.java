f0b8a4ad34a0bc0e151a2a0cae00f31281058e53
Daniel Parkin <danielparkin1@gmail.com>
2018-04-14 14:27:33 +0100

    add introduce local extension

--- src/test/IntroduceForeignMethodTests.java
+++ src/test/IntroduceForeignMethodTests.java
@@ -7,7 +7,7 @@ import static junit.framework.TestCase.assertEquals;
 
 public class IntroduceForeignMethodTests {
     @Test
-    public void next_day_increments_date_by_1_day() {
+    public void roll_over_increments_date_by_1_day() {
         Date expectedDate = new Date(2018, 04, 14);
         Date actualDate = new BillingPeriod(new Date(2018, 04, 13)).rollOver();
         assertEquals(expectedDate.toString(), actualDate.toString());
